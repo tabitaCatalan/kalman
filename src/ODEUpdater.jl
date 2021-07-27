43e7b92737ccc86953b807e025bbfc0184c58cb7
Tabita <tabicm.nhg@gmail.com>
2021-07-27 12:38:15 -0400

    Change: ODEUpdater uses contdiscrete, forecast returns component array

--- src/ODEUpdater.jl
+++ src/ODEUpdater.jl
@@ -6,8 +6,7 @@ using LinearAlgebra
 mutable struct ODEForecaster <: KalmanFilter.KalmanUpdater
     dt 
     n_steps::Int 
-    Xn::ComponentArray # creo que es innececsario
-    discret_system::KalmanFilter.Discretizer
+    #Xn::ComponentArray # creo que es innececsario
     discret_momentum::KalmanFilter.Discretizer
     """Función que corrige un estado `x` para dejarlo dentro de cierto dominio."""
     integrity
@@ -16,14 +15,13 @@ end
 """
 Outer constructor
 """
-function ODEForecaster(dt, n_steps, x0, P0, F, f, Dxf, p, integrity)
+function ODEForecaster(dt, n_steps, x0, P0, p, integrity, momentum::ContinuousDiscretMomentum)
     small_dt = dt/n_steps
-    discret_system = KalmanFilter.RK4Dx(f, Dxf, p, small_dt)
-    Q = Diagonal(sqrt(small_dt) * ones(length(x0)))
-    X0 = ComponentArray(x=x0, P= P0)
-    momentum_eq = (X, a, p) -> momentum_equation(discret_system, X.x, X.P, a, p, F(X.x), Q)
-    discret_momentum = KalmanFilter.SimpleRK4(momentum_eq, p, small_dt)
-    ODEForecaster(dt, n_steps, X0, discret_system, discret_momentum, integrity)
+    #discret_system = KalmanFilter.RK4Dx(f, Dxf, p, small_dt)
+    #Q = Diagonal(sqrt(small_dt) * ones(length(x0)))
+    #X0 = ComponentArray(x=x0, P= P0)
+    discret_momentum = KalmanFilter.SimpleRK4(momentum, p, small_dt)
+    ODEForecaster(dt, n_steps, discret_momentum, integrity)
 end 
 
 # tengo que asegurar que el discretizer usa un dt más chico... 
@@ -33,7 +31,7 @@ end
 function next_Xn(Xn, odefor::ODEForecaster, control)
     odefor.discret_momentum(Xn, control)
 end 
-
+#=
 function set_Xn!(odefor::ODEForecaster, Xn)
     set_Xn!(odefor.Xn, Xn)
 end 
@@ -42,23 +40,29 @@ function set_Xn!(Xn::ComponentArray, Xnew)
     Xn.x .= Xnew.x
     Xn.P .= Xnew.P    
 end
+=#
 
-## Para darle la interfaz de KalmanUpdater 
+#= Para darle la interfaz de KalmanUpdater 
 function update!(odefor::ODEForecaster, hatx, hatP, control) 
     set_Xn!(odefor, ComponentArray(x = hatx, P = hatP))
+end =#
+function update!(odefor::ODEForecaster, hatx, hatP, control) end
+
+function apply_integrity!(X, odefor)
+    X.x .= odefor.integrity(X.x)
 end
 
 function forecast(odefor::ODEForecaster, hatx, hatP, control)
-    X_actual = ComponentArray(x = hatx, P = hatP)
+    X_actual = ComponentArray(x = hatx, P = hatP, Ck = hatP)
     for n in 1:odefor.n_steps
-        set_Xn!(X_actual, next_Xn(X_actual, odefor, control))
+        X_actual = next_Xn(X_actual, odefor, control)
     end 
-    odefor.integrity(X_actual.x), X_actual.P
+    apply_integrity!(X_actual, odefor)
+    X_actual
 end 
 # confío en que no la van a llamar... solo debería usarse con measurements,
 # NO con InnerState  
 function (updater::ODEForecaster)(x::AbstractArray, u::Real, error)end
-
 # Necesito agregar una ODE para el sistema de la matriz P 
 # ```math
 # dP/dt = \frac{\partial F(t, \hat{x})}{\partial \hat{x}} P + P \left( \frac{\partial F(t, \hat{x})}{\partial \hat{x}} \right)^{T} + G(t) Q(t) G^{T}(t)
@@ -86,11 +90,6 @@ function momentum(ds::KalmanFilter.Discretizer, F, Q)
 end 
 =#
 
-function momentum_equation(ds::KalmanFilter.Discretizer, x, P, a, p, Fx, Q)
-    dx = ds.f(x, a, p)
-    dxFP = ds.Dxf(x, a, p) * P
-    ComponentArray(x = dx, P = dxFP + dxFP' + Fx * Q * Fx')
-end 
 
 
 
