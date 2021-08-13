e04cc9ff6980afa6d49d87eb233cce83a8ade675
Tabita <tabicm.nhg@gmail.com>
2021-08-13 11:31:18 -0400

    feat!: Discretizer admits functions that depend on time (t)
    Changes in `Discretizer` interface. Now, it accepts functions of the form `f(x, \alpha, p, t)` (added support for `t`). 
    
    Chores: modify Updaters which use Discretizers in accordance with the new interface.
    Fix: #2

--- docs/src/discretizers.md
+++ docs/src/discretizers.md
@@ -1,14 +1,14 @@
-# Discretizadores: Discretizer 
+# Discretizadores: `Discretizer `
 
 El tipo abstracto `Discretizer` está pensado como una interfaz a las estructuras
 que permitan trabajar con una Ecuación Diferencial Ordinaria autónoma (un sistema continuo)
 de la forma 
 ```math
-x'(t) = f_p(x(t), \alpha(t))
+x'(t) = f_p(x(t), \alpha(t), t)
 ```
 donde ``\alpha`` es una función de ``t`` conocida, y transformarlo en uno discreto
 ```math
-x_{n+1} = \mathcal{M}_p(x_n, \alpha_n)
+x_{n+1} = \mathcal{M}_p(x_n, \alpha_n, t_n)
 ```
 El subíndice ``p`` denota la posible dependencia de  ``f`` de algunos parámetros.
 Esto se hace mediante métodos numéricos como Euler, Runge Kutta, etc.
@@ -17,19 +17,19 @@ Para que funcione correctamente deben implementarse los siguientes métodos:
 
 Métodos a implementar | Breve descripción
 --- | ---
-`(::Discretizer)(x::AbstractArray,u::Real) ` | El método debe poder ser evaluado en un estado y un control
-`jacobian_x(ds::Discretizer, x, α)` | Debe contarse con la derivada con respecto al estado.
+`(::Discretizer)(x::AbstractArray, α::Real, t) ` | El método debe poder ser evaluado en un estado ``x``, un control ``\alpha`` y en tiempo ``t``.
+`jacobian_x(ds::Discretizer, x, α, t)` | Debe contarse con la derivada con respecto al estado.
 
 Usaremos Euler progresivo como un ejemplo de cómo implementar la interfaz. 
 
 
-## Método Euler progresivo 
+## Ejemplo de implementación: Método Euler progresivo 
 
 Dada una condición inicial ``x(0) = x_0``, y un paso de tiempo ``\Delta t``, definimos ``t_{n+1} = t_{n} + \Delta t``.
 Denotamos ``\alpha_n := \alpha(t_n)``.
 Definimos la sucesión ``\{x_n \}_n``, intentando que ``x_n \approx x(t_{n+1})``, mediante la recursión
 ```math
-x_{n+1} = x_n + \Delta t f_p(x_n, \alpha_n) =: \mathcal{M}_p(x_n, \alpha_n)
+x_{n+1} = x_n + \Delta t f_p(x_n, \alpha_n, t_n) =: \mathcal{M}_p(x_n, \alpha_n, t_n)
 ```
 Para definir el discretizador, usaremos además la información del diferencial con respecto a ``x`` de ``f_p``, ya que
 esto es útil más adelante a la hora de definir un filtro de kalman extendido.
@@ -40,10 +40,18 @@ KalmanFilter.Euler
 
 Esto permite definir los dos métodos necesarios de la interfaz 
 ```julia
-(eu::Euler)(x, α) = x + eu.dt * eu.f(x, α , eu.p)
+(eu::Euler)(x, α, t) = x + eu.dt * eu.f(x, α , eu.p, t)
 ```
 ```julia
-jacobian_x(eu::Euler, x, α) = I + eu.dt * eu.Dxf(x, α, eu.p)
+jacobian_x(eu::Euler, x, α, t) = I + eu.dt * eu.Dxf(x, α, eu.p, t)
 ```
 Donde `I` es la matriz identidad dada por el paquete `LinearAlgebra`.
 
+## `Discretizer`s disponibles 
+
+`Discretizer` | Descripción 
+---|--- 
+`Euler` | Método de Euler progresivo, que permite ser diferenciado c/r a la variable ``x``.
+`RK4Dx` | Método de Runge Kutta de orden 4, que permite ser diferenciado c/r a la variable ``x``. 
+
+Agregar constructores de todos los discretizers disponibles. <!-- TODO -->
--- src/discretizers.jl
+++ src/discretizers.jl
@@ -3,8 +3,8 @@ using LinearAlgebra
 
 abstract type Discretizer end
 
-function (::Discretizer)(x,α) error("No se ha definido un método para este discretizador.") end
-function jacobian_x(ds::Discretizer, x, α) error("No se ha definido un método para este discretizador.")  end
+function (::Discretizer)(x,α,t) error("No se ha definido un método para este discretizador.") end
+function jacobian_x(ds::Discretizer, x, α, t) error("No se ha definido un método para este discretizador.")  end
 
 """
 $(TYPEDEF)
@@ -14,11 +14,11 @@ $(FIELDS)
 """
 struct Euler <: Discretizer
   """
-  La función tal que ``x' = f(x,\\alpha, p)``, donde ``x`` es el estado,
-  ``\\alpha `` un control, y ``p`` son parámetros extra.
+  La función tal que ``x' = f(x,\\alpha, p, t)``, donde ``x`` es el estado,
+  ``\\alpha `` un control, ``p`` son parámetros extra y ``t`` es el tiempo.
   """
   f
-  """``D_x f (x,\\alpha, p)``, el diferencial de ``f`` con respecto a ``x``."""
+  """``D_x f (x,\\alpha, p, t)``, el diferencial de ``f`` con respecto a ``x``."""
   Dxf
   """Parámetros extra ``p``."""
   p
@@ -26,8 +26,8 @@ struct Euler <: Discretizer
   dt
 end
 
-(eu::Euler)(x, α) = x + eu.dt * eu.f(x, α , eu.p)
-jacobian_x(eu::Euler, x, α) = I + eu.dt * eu.Dxf(x, α, eu.p)
+(eu::Euler)(x, α, t) = x + eu.dt * eu.f(x, α , eu.p, t)
+jacobian_x(eu::Euler, x, α, t) = I + eu.dt * eu.Dxf(x, α, eu.p, t)
 
 abstract type RK4 <: Discretizer end
 
@@ -39,8 +39,8 @@ $(FIELDS)
 """
 struct SimpleRK4 <: RK4
   """
-  La función tal que ``x' = f(x,\\alpha, p)``, donde ``x`` es el estado,
-  ``\\alpha `` un control, y ``p`` son parámetros extra.
+  La función tal que ``x' = f(x,\\alpha, p, t)``, donde ``x`` es el estado,
+  ``\\alpha `` un control, ``p`` son parámetros extra y ``t`` es el tiempo.
   """
   f
   """Parámetros extra ``p``."""
@@ -58,11 +58,11 @@ $(FIELDS)
 """
 struct RK4Dx <: RK4
   """
-  La función tal que ``x' = f(x,\\alpha, p)``, donde ``x`` es el estado,
-  ``\\alpha `` un control, y ``p`` son parámetros extra.
+  La función tal que ``x' = f(x,\\alpha, p, t)``, donde ``x`` es el estado,
+  ``\\alpha `` un control, ``p`` son parámetros extra y ``t`` es el tiempo.
   """
   f
-  """``D_x f (x,\\alpha, p)``, el diferencial de ``f`` con respecto a ``x``."""
+  """``D_x f (x,\\alpha, p, t)``, el diferencial de ``f`` con respecto a ``x``."""
   Dxf
   """Parámetros extra ``p``."""
   p
@@ -70,41 +70,41 @@ struct RK4Dx <: RK4
   dt
 end
 
-function get_ks(rk::RK4, x, α)
+function get_ks(rk::RK4, x, α, t)
   p = rk.p; dt = rk.dt
 
   x1 = x
-  k1 = rk.f(x1, α, p)
+  k1 = rk.f(x1, α, p, t)
 
   x2 = x1 + dt * k1/2
-  k2 = rk.f(x2, α, p)
+  k2 = rk.f(x2, α, p, t)
 
   x3 = x1 + dt * k2/2
-  k3 = rk.f(x3, α, p)
+  k3 = rk.f(x3, α, p, t)
 
   x4 = x1 + dt * k3
-  k4 = rk.f(x4, α, p)
+  k4 = rk.f(x4, α, p, t)
   (x1, x2, x3, x4), (k1, k2, k3, k4)
 end
 
-function (rk::RK4)(x,α)
+function (rk::RK4)(x,α,t)
 
-  xs, ks = get_ks(rk, x, α)
+  xs, ks = get_ks(rk, x, α, t)
 
   x + (rk.dt/6) * (ks[1] + 2 * ks[2] + 2 * ks[3] + ks[4])
 end
 
-function jacobian_x(rk::RK4, x, α)
+function jacobian_x(rk::RK4, x, α, t)
   dt = rk.dt; p = rk.p
-  xs, ks = get_ks(rk, x, α)
+  xs, ks = get_ks(rk, x, α, t)
 
-  Dₓk₁ = rk.Dxf(xs[1], α, p)
+  Dₓk₁ = rk.Dxf(xs[1], α, p, t)
   Dₓx₂ = I + (dt/2) * Dₓk₁
-  Dₓk₂ = rk.Dxf(xs[2], α, p) * Dₓx₂
+  Dₓk₂ = rk.Dxf(xs[2], α, p, t) * Dₓx₂
   Dₓx₃ = I + (dt/2) * Dₓk₂
-  Dₓk₃ = rk.Dxf(xs[3], α, p) * Dₓx₃
+  Dₓk₃ = rk.Dxf(xs[3], α, p, t) * Dₓx₃
   Dₓx₄ = I + dt * Dₓk₃
-  Dₓk₄ = rk.Dxf(xs[4], α, p) * Dₓx₄
+  Dₓk₄ = rk.Dxf(xs[4], α, p, t) * Dₓx₄
 
   I + (dt/6) * (Dₓk₁ + 2 * Dₓk₂ + 2 * Dₓk₃ + Dₓk₄)
 end
